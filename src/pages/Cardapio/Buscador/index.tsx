a75908d6fce88759c7fafbd6754d57109c7285b5
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-16 22:53:48 -0300

    npm i react-icons

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -1,4 +1,5 @@
 import styles from './Buscador.module.scss'
+import { CgSearch } from 'react-icons/cg'
 
 interface BuscadorProps {
     busca: string,
@@ -9,6 +10,7 @@ export default function Buscador({busca, setBusca} :BuscadorProps) {
     return (
         <div className={styles.buscador}>
             <input type="text" value={busca} onChange={evento => setBusca(evento.target.value)}/>
+            <CgSearch size={20} color="#4c4d5e" />
         </div>
     )
 }
