a0a13fe12094ada75a364e3ba56e933e56b59f4a
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-17 22:30:56 -0300

    adicionado filtro

---
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -0,0 +1,23 @@
+import filtros from './filtros.json'
+import styles from './Filtros.module.scss'
+
+type IOpcao = {
+    id: number,
+    label: string
+}
+
+export default function Filtros() {
+    function selecionarFiltro(opcao: IOpcao) {
+
+    }
+
+    return (
+        <div className={styles.filtros}>
+            {filtros.map( opcao => (
+                <button  className={styles.filtros__filtro} key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
+                    {opcao.label}
+                </button>
+            ))}
+        </div>
+    )
+}
