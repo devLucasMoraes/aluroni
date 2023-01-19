5bac5679219d0230fa3a161bb5454e1d3ef7065a
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-18 22:37:55 -0300

    buscador e filtros

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,13 +1,39 @@
+import { useEffect, useState } from 'react';
 import Item from './Item'
 import cardapio from './itens.json'
 import styles from './Itens.module.scss'
 
-export default function Itens() {
+interface ItensProps {
+    busca: string,
+    filtro: number | null;
+    ordenador: string
+}
+
+export default function Itens( props : ItensProps) {
+    const [lista, setLista] = useState(cardapio);
+    const { busca, filtro } = props;
+
+    function testaBusca(title: string) {
+        const regex = new RegExp(busca, 'i');
+        return regex.test(title);
+    }
+
+    function testaFiltro(id: number) {
+        if (filtro !== null) return filtro === id;
+        return true;
+    }
+
+    useEffect(() => {
+        const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+        setLista(novaLista);
+    }, [busca, filtro])
+
     return (
         <div className={styles.itens}>
-            { cardapio.map(item => (
-                <Item key={item.id} {...item}/>
+            {lista.map(item => (
+                <Item key={item.id} {...item} />
             ))}
         </div>
     )
 }
+
