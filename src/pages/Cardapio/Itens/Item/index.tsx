d3d4de56b77db0f2344914f960d52aedab4b978a
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-18 21:43:36 -0300

    utilizando o classnames

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,6 +1,7 @@
 import styles from './Item.module.scss'
 import logo from 'assets/logo.svg'
 import cardapio from '../itens.json'
+import classNames from 'classnames'
 
 type ItemProps = typeof cardapio[0]
 
@@ -18,7 +19,10 @@ export default function Item(props: ItemProps) {
                     <p> {description} </p>
                 </div>
                 <div className={styles.item__tags}>
-                    <div className={styles.item__tipo}>
+                    <div className={classNames({
+                        [styles.item__tipo] : true,
+                        [styles[`item__tipo__${category.label.toLowerCase()}`]] : true
+                    })}>
                         {category.label}
                     </div>
                     <div className={styles.item__porcao}>
