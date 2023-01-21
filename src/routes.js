1ff471921839efd85c00bcefec908c3f93380530
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-20 22:12:05 -0300

    reutilizando o header com rotas aninhadas

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,21 @@
 import Menu from 'components/Menu';
+import { PaginaPadrao } from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function AppRouter() {
     return (
-        <BrowserRouter>
-            <Menu/>
-            <Routes>
-                <Route path='/' element={<Inicio />}/>
-                <Route path='/cardapio' element={<Cardapio />}/>
-            </Routes>
-        </BrowserRouter>
+        <main>
+            <BrowserRouter>
+                <Menu />
+                <Routes>
+                    <Route path='/' element={<PaginaPadrao />}>
+                        <Route index element={<Inicio />} />
+                        <Route path='/cardapio' element={<Cardapio />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </main>
     );
 }
