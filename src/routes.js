31a3e482e19388aae4dbf8a28811d7a8a43b1036
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-20 21:03:54 -0300

    reaproveitando menu com react-router

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import Menu from 'components/Menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -5,6 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 export default function AppRouter() {
     return (
         <BrowserRouter>
+            <Menu/>
             <Routes>
                 <Route path='/' element={<Inicio />}/>
                 <Route path='/cardapio' element={<Cardapio />}/>
